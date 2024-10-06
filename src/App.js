1e1ada383b13d7c567d3b2125cee57fe57bdd4fc
Mamun0707 <abdulalmamun637@gmail.com>
2024-10-06 13:46:18 +0600

    dashboard edit

--- src/App.js
+++ src/App.js
@@ -1,133 +1,133 @@
-import React, { useState, useEffect } from 'react';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Employee from './pages/Employee';
-import EmployeeAdd from './pages/Employee/EmployeeAdd';
-import Project from './pages/Project';
-import Attendance from './pages/Attendance';
-import Clients from './pages/Clients';
-import LeaveManagement from './pages/LeaveManagement';
-import Holidays from './pages/Holidays';
-import Accounts from './pages/Accounts';
-import Departments from './pages/Departments';
-import Payroll from './pages/Payroll';
-import Job from './pages/Job';
-import Consultancy from './pages/Consultancy';
-import Forms from './pages/Forms';
-import Designation from './pages/Designation';
-import DesignationAdd from './pages/Designation/DesignationAdd';
-import Protected from './components/protected';
-// import Fahim from './pages/Fahim';
-// import Ramjan from './pages/Ramjan';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+      import React, { useState, useEffect } from 'react';
+      import Dashboard from './pages/Dashboard';
+      import Login from './pages/Login';
+      import Register from './pages/Register';
+      import Employee from './pages/Employee';
+      import EmployeeAdd from './pages/Employee/EmployeeAdd';
+      import Project from './pages/Project';
+      import Attendance from './pages/Attendance';
+      import Clients from './pages/Clients';
+      import LeaveManagement from './pages/LeaveManagement';
+      import Holidays from './pages/Holidays';
+      import Accounts from './pages/Accounts';
+      import Departments from './pages/Departments';
+      import Payroll from './pages/Payroll';
+      import Job from './pages/Job';
+      import Consultancy from './pages/Consultancy';
+      import Forms from './pages/Forms';
+      import Designation from './pages/Designation';
+      import DesignationAdd from './pages/Designation/DesignationAdd';
+      import Protected from './components/protected';
+      // import Fahim from './pages/Fahim';
+      // import Ramjan from './pages/Ramjan';
+      import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
-function App() {
-  const isSignedIn = localStorage.getItem("access_token") || false;
-  return (
-    <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<Dashboard />} /> */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path={"/"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <Dashboard />
-          </Protected>
-        } />
-          <Route path={"/employee/add"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <EmployeeAdd />
-          </Protected>
-        } />
-          <Route path={"/Employee"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <Employee />
-          </Protected>
-        } />
-        <Route path={"/employee/edit/:id"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <EmployeeAdd />
-          </Protected>
-        } />
-          <Route path={"/Project"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <Project />
-          </Protected>
-        } />
-          <Route path={"/Attendance"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <Attendance />
-          </Protected>
-        } />
-          <Route path={"/Clients"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <Clients />
-          </Protected>
-        } />
-          <Route path={"/LeaveManagement"} element={
-          <Protected isSignedIn={isSignedIn} >
-            <LeaveManagement />
-          </Protected>
-        } />
-         <Route path={"/Holidays"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Holidays />
-            </Protected>
-          } />
-         <Route path={"/Accounts"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Accounts />
-            </Protected>
-          } />
-         <Route path={"/Departments"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Departments />
-            </Protected>
-          } />
-         <Route path={"/Payroll"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Payroll />
-            </Protected>
-          } />
-         <Route path={"/Job"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Job />
-            </Protected>
-          } />
-         <Route path={"/Consultancy"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Consultancy />
-            </Protected>
-          } />
-         <Route path={"/Forms"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Forms />
-            </Protected>
-          } />
-          <Route path={"/designation"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <Designation />
-            </Protected>
-          } />
-         <Route path={"/designation/add"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <DesignationAdd />
-            </Protected>
-          } />
-          <Route path={"/designation/edit/:id"} element={
-            <Protected isSignedIn={isSignedIn} >
-              <DesignationAdd />
-            </Protected>
-          } />
-          {/* <Route path="/fahim" element={<Fahim />} />
-          <Route path="/ramjan" element={<Ramjan />} /> */}
-        </Routes>
-      </BrowserRouter>
-    
-    
-  );
-}
+      function App() {
+        const isSignedIn = localStorage.getItem("access_token") || false;
+        return (
+          <BrowserRouter>
+              <Routes>
+                {/* <Route path="/" element={<Dashboard />} /> */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path={"/"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <Dashboard />
+                </Protected>
+              } />
+                <Route path={"/employee/add"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <EmployeeAdd />
+                </Protected>
+              } />
+                <Route path={"/Employee"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <Employee />
+                </Protected>
+              } />
+              <Route path={"/employee/edit/:id"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <EmployeeAdd />
+                </Protected>
+              } />
+                <Route path={"/Project"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <Project />
+                </Protected>
+              } />
+                <Route path={"/Attendance"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <Attendance />
+                </Protected>
+              } />
+                <Route path={"/Clients"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <Clients />
+                </Protected>
+              } />
+                <Route path={"/LeaveManagement"} element={
+                <Protected isSignedIn={isSignedIn} >
+                  <LeaveManagement />
+                </Protected>
+              } />
+              <Route path={"/Holidays"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Holidays />
+                  </Protected>
+                } />
+              <Route path={"/Accounts"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Accounts />
+                  </Protected>
+                } />
+              <Route path={"/Departments"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Departments />
+                  </Protected>
+                } />
+              <Route path={"/Payroll"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Payroll />
+                  </Protected>
+                } />
+              <Route path={"/Job"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Job />
+                  </Protected>
+                } />
+              <Route path={"/Consultancy"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Consultancy />
+                  </Protected>
+                } />
+              <Route path={"/Forms"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Forms />
+                  </Protected>
+                } />
+                <Route path={"/designation"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <Designation />
+                  </Protected>
+                } />
+              <Route path={"/designation/add"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <DesignationAdd />
+                  </Protected>
+                } />
+                <Route path={"/designation/edit/:id"} element={
+                  <Protected isSignedIn={isSignedIn} >
+                    <DesignationAdd />
+                  </Protected>
+                } />
+                {/* <Route path="/fahim" element={<Fahim />} />
+                <Route path="/ramjan" element={<Ramjan />} /> */}
+              </Routes>
+            </BrowserRouter>
+          
+          
+        );
+      }
 
-export default App;
+      export default App;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,7 +5,7 @@ function Dashboard() {
   return (
     <AdminLayout>
         <>
-        <div className="row  border-bottom white-bg dashboard-header">
+        {/* <div className="row  border-bottom white-bg dashboard-header">
 
                 <div className="col-md-3">
                     <h2>Welcome HRM</h2>
@@ -375,7 +375,185 @@ function Dashboard() {
               </div>
 
           </div>
-      </div>
+      </div> */}
+
+{/* <div class="container-xxl flex-grow-1 container-p-y">
+              
+              <div class="row">
+                  <div class="col-lg-12 mb-4 order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                        <div class="col-sm-7">
+                          <div class="card-body">
+                            <h2 class="card-title text-primary">Welcome To Medicine Store!</h2>
+                            <p class="mb-4">
+                              <span class="fw-medium"></span> 
+                            </p>
+                          </div>
+                        </div>
+                        <div class="col-sm-5 text-center text-sm-left">
+                          <div class="card-body pb-0 px-0 px-md-4">
+                            <img
+                              src="<?= $baseurl ?>/assets/img/illustrations/medicine.png"
+                              height="250"
+                              alt="View Badge User"
+                               />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+                  <div class="row">
+  
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-person-workspace"></i></h1>
+                            <a href="supplier_add.php"><h5 class="card-title text-center">Suppliers</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-person-plus-fill"></i></h1>
+                            <a href="customer_add.php"><h5 class="card-title text-center">Customer</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-capsule"></i></h1>
+                            <a href="medicine_add.php"><h5 class="card-title text-center">Medicine</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-bag-fill"></i></i></h1>
+                            <a href="purchase_add.php"><h5 class="card-title text-center">Purchase</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  
+            
+                  </div>
+  
+  
+                  <div class="row mt-5">
+  
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-cart3"></i></h1>
+                            <a href="sales_add.php"><h5 class="card-title text-center">Sales</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-bag-fill"></i></h1>
+                            <a href="purchase_return.php"><h5 class="card-title text-center">Purchase Return</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-cart3"></i></h1>
+                            <a href="sales_return.php"><h5 class="card-title text-center">Sales Return</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-journal-medical"></i></h1>
+                            <a href="stock_list.php"><h5 class="card-title text-center">Report</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+            
+                  </div>
+                  
+                
+                  <div class="row mt-5">
+  
+                  <div class="col-lg-3  order-0">
+                      <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi-tags-fill"></i></h1>
+                            <a href="coupon_add.php"><h5 class="card-title text-center">Coupon</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                      <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-bag-heart"></i></h1>
+                            <a href="orders_list.php"><h5 class="card-title text-center">Orders</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-person-arms-up"></i></h1>
+                            <a href="user_add.php"><h5 class="card-title text-center">Users</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    <div class="card">
+                      <div class="d-flex align-items-end row">
+                          <div class="card-body text-center">
+                            <h1><i class="menu-icon bi bi-prescription2"></i></h1>
+                            <a href="type_add.php"><h5 class="card-title text-center">Medicine Type</h5></a> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-3  order-0">
+                    
+                    </div>
+            
+                  </div> */}
+                
+                
+  
+  
+         
+          
+   
         </>
     </AdminLayout>       
   )
