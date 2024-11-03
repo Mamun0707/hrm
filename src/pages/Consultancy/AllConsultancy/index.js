5414094e185b33ba6102762682b0b89ba154f42c
Mamun0707 <abdulalmamun637@gmail.com>
2024-11-03 09:45:00 +0600

    commit

---
+++ src/pages/Consultancy/AllConsultancy/index.js
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AdminLayout from '../../../layouts/AdminLayout';
+import { useNavigate } from 'react-router-dom';
+import {useParams} from "react-router-dom";
+
+function AllConsultancy() {
+    const [inputs, setInputs] = useState({id:'',name:'',cont_per:'',phone:'',email:'',address:'',cont_date:'',cont_for:''});
+    const navigate=useNavigate();
+    const {id} = useParams();
+    
+    function getDatas(){
+        axios.get(`${process.env.REACT_APP_API_URL}/resignation/${id}`).then(function(response) {
+            setInputs(response.data.data);
+        });
+    }
+
+    useEffect(() => {
+        if(id){
+            getDatas();
+        }
+    }, []);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        console.log(inputs)
+        
+        try{
+            let apiurl='';
+            if(inputs.id!=''){
+                apiurl=`/resignation/edit/${inputs.id}`;
+            }else{
+                apiurl=`/resignation/create`;
+            }
+            
+            let response= await axios({
+                method: 'post',
+                responsiveTYpe: 'json',
+                url: `${process.env.REACT_APP_API_URL}${apiurl}`,
+                data: inputs
+            });
+            navigate('/resignation')
+        } 
+        catch(e){
+            console.log(e);
+        }
+    }
+  return (
+    <AdminLayout>
+        <div className="main-content container-fluid">
+            <div className="page-title">
+                <div className="row">
+                    <div className="col-12 col-md-6 order-md-1 order-last">
+                        <h3>Add New Resignation</h3>
+                    </div>
+                    <div className="col-12 col-md-6 order-md-2 order-first">
+                        <nav aria-label="breadcrumb" className='breadcrumb-header'>
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                                <li className="breadcrumb-item active" aria-current="page">Add New</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+            <section id="basic-vertical-layouts">
+                <div className="row match-height">
+                    <div className="col-12">
+                        <div className="card">
+                            <div className="card-content">
+                                <div className="card-body">
+                                    <form className="form form-vertical" onSubmit={handleSubmit}>
+                                        <div className="form-body">
+                                            <div className="row">
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="first-name-vertical">Name</label>
+                                                    <input type="text" id="first-name-vertical" className="form-control" defaultValue={inputs.name} name="name" onChange={handleChange} placeholder="Full Name"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">Contact Person</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.cont_per} name="cont_per" onChange={handleChange} placeholder=""/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">Phone </label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.phone} name="phone" onChange={handleChange} placeholder="Phone"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">Email</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.email} name="email" onChange={handleChange} placeholder="email"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">Address</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.address} name="address" onChange={handleChange} placeholder="Address"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">Contact Date</label>
+                                                    <input type="date" id="email-id-vertical" className="form-control" defaultValue={inputs.cont_date} name="cont_date" onChange={handleChange} placeholder=""/>
+                                                    </div>
+                                                </div>
+                                                
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">Contact For</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.cont_for} name="cont_for" onChange={handleChange} placeholder=""/>
+                                                    </div>
+                                                </div>
+                                                
+                                                <div className="col-12 d-flex justify-content-end">
+                                                    <button type="submit" className="btn btn-primary mr-1 mb-1">Submit</button>
+                                                    <button type="reset" className="btn btn-light-secondary mr-1 mb-1">Reset</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+    </AdminLayout>    
+  )
+}
+
+export default AllConsultancy
